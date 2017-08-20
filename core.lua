b54313b95e5ce1d5df000ea8aa4f2143d4c80508
phyber <phyber@localhost>
2017-08-20 13:46:38 +0100

    Don't try to UnitSetRole while in combat.

--- core.lua
+++ core.lua
@@ -1,32 +1,36 @@
 local AutoRole = CreateFrame("Frame", nil, UIParent)
 
 -- WoW functions/constants
-local UnitSetRole = UnitSetRole
 local GetSpecialization = GetSpecialization
 local GetSpecializationInfo = GetSpecializationInfo
 local StaticPopupSpecial_Hide = StaticPopupSpecial_Hide
+local UnitAffectingCombat = UnitAffectingCombat
+local UnitSetRole = UnitSetRole
 
 AutoRole:RegisterEvent("ROLE_POLL_BEGIN")
 AutoRole:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 
 local function AutoRole_SetRole(frame, event, ...)
-	-- Get our current spec.
-	local specIndex = GetSpecialization()
+    -- Check if we're in combat as UnitSetRole is protected inCombat.
+    local inCombat = UnitAffectingCombat("player")
 
-	-- It's possible to not know any specs.
-	if specIndex then
-		-- Find out what the role of the spec is.
-		-- specID, specName, specDesc, specIcon, specBackground, specRole
-		local _, _, _, _, _, specRole = GetSpecializationInfo(specIndex)
+    -- Get our current spec.
+    local specIndex = GetSpecialization()
 
-		-- Set our current role.
-		UnitSetRole("player", specRole)
+    -- It's possible to not know any specs.
+    if specIndex and not inCombat then
+        -- Find out what the role of the spec is.
+        -- specID, specName, specDesc, specIcon, specBackground, specRole
+        local _, _, _, _, _, specRole = GetSpecializationInfo(specIndex)
 
-		-- Close the Role Poll popup
-		if event == "ROLE_POLL_BEGIN" then
-			StaticPopupSpecial_Hide(RolePollPopup)
-		end
-	end
+        -- Set our current role.
+        UnitSetRole("player", specRole)
+
+        -- Close the Role Poll popup
+        if event == "ROLE_POLL_BEGIN" then
+            StaticPopupSpecial_Hide(RolePollPopup)
+        end
+    end
 end
 
 AutoRole:SetScript("OnEvent", AutoRole_SetRole)
